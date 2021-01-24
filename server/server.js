a601674f5fe6d9c3f27d220bc60edb7469bac941
henryngann <henrynganwork@gmail.com>
2021-01-23 21:46:58 -0500

    post update

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const db = require("./db");
+const cors = require("cors");
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 //Get All Restaurants
 app.get("/api/v1/restaurants", async (req, res) => {
   try {
     const results = await db.query("select * from restaurants");
-    console.log(results);
+
     res.json({
       status: "success",
       results: results.rows.length,
@@ -40,13 +41,12 @@ app.get("/api/v1/restaurants/:id", async (req, res) => {
 });
 //create a restaurant(POST)
 app.post("/api/v1/restaurants/", async (req, res) => {
-  console.log(req.body);
   try {
     const results = await db.query(
       "INSERT INTO restaurants(name,location,price_range)values($1,$2,$3) returning *",
       [req.body.name, req.body.location, req.body.price_range]
     );
-    console.log(results);
+
     res.status(201).json({
       status: "success",
       data: {
@@ -65,7 +65,7 @@ app.put("/api/v1/restaurants/:id", async (req, res) => {
       "UPDATE restaurants SET name = $1, location = $2, price_range = $3 where id = $4 returning *",
       [req.body.name, req.body.location, req.body.price_range, req.params.id]
     );
-    console.log(results, "this is results");
+
     res.status(200).json({
       status: "success",
       data: {
